d8efc6b60506c4b8b4be82f65af2860ecb5ebdd5
futter-krot <66520330+futter-krot@users.noreply.github.com>
2021-07-07 21:04:22 +0300

    Update auth_test.py

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,35 +1,34 @@
 import pytest, pytest_cov
-from ..gallows import Gallow, choose_word, calculate_score, create_game, choices, attempts
 from .. import gallows
 
 class TestClass:
 
     def test_round(self):
-        game = Gallow()
+        game = gallows.Gallow()
         assert game.round() == None
 
     def test_input(self):
         gallows.input = lambda _: 'ans'
-        output = Gallow.round
-        assert output == Gallow.round
+        output = gallows.Gallow.round
+        assert output == gallows.Gallow.round
 
     def teardown_method(self, method):
         gallows.input = input
 
     def test_choose_word(self):
-        random_word = choose_word(choices)
-        assert random_word in choices
+        random_word = gallows.choose_word(gallows.choices)
+        assert random_word in gallows.choices
 
     def test_calcualte_score_positive(self):
-        score = calculate_score(1)
+        score = gallows.calculate_score(1)
         assert score == 3
 
     def test_calcualte_score_negative(self):
-        score = calculate_score(5)
+        score = gallows.calculate_score(5)
         assert score == 0
 
     def test_create_game(self):
-        game = create_game()
-        assert game.word in choices
+        game = gallows.create_game()
+        assert game.word in gallows.choices
         assert game.attempts == 4
         assert game.cypher == '-' * len(game.word)
